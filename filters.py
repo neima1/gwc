b84946a78b43c99be4069b7598f1c55a8819d222
neima1 <41390883+neima1@users.noreply.github.com>
2018-07-18 14:02:47 -0500

    Filters2

---
+++ filters.py
@@ -0,0 +1,55 @@
+from PIL import Image
+
+
+
+    #newimg = filters.obamicon(img)
+    #filters.save_img(newimg, "recolored.jpg")
+
+
+def load_img(filename):
+  im = Image.open(filename)
+  show_img(im,filename)
+
+def show_img(im,filename):
+  im.show()
+  return im
+
+  #save_img(im,filename)
+
+def save_img(im, filename):
+  im.Image.save(filename, "jpeg")
+
+
+def obamicon(im):
+  # Load the pixel data from im.
+  pixels = im.getdata()
+  # Create a list to hold the new image pixel data.
+  new_pixels = []
+
+  # Define color constants to use for recoloring.
+  darkBlue = (0, 51, 76)
+  red = (217, 26, 33)
+  lightBlue = (112, 150, 158)
+  yellow = (252, 227, 166)
+
+  # Process the pixels in the image.
+  for p in pixels:
+    # Pixel intensity = R value + G value + B value
+    intensity = p[0] + p[1] + p[2]
+
+    if intensity < 182:
+      new_pixels.append(darkBlue)
+
+    elif intensity >= 182 and intensity < 364:
+      new_pixels.append(red)
+
+    elif intensity >= 364 and intensity < 546:
+      new_pixels.append(lightBlue)
+
+    elif intensity >=546:
+      new_pixels.append(yellow)
+
+  # Save the filtered pixels as a new image
+  newim = Image.new("RGB", im.size)
+  newim.putdata(new_pixels)
+  return newim
