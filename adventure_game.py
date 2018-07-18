02284465997a54f01f5759e3fe0cefa31bcd6555
neima1 <41390883+neima1@users.noreply.github.com>
2018-07-18 13:53:28 -0500

    Adventure game

---
+++ adventure_game.py
@@ -0,0 +1,27 @@
+startgame = input("I have a day off! Should I go to the 'beach', the 'park', or the 'city'?")
+  if (startgame == "beach"):
+    firstturn = input("Finally! A break! Should I go 'swim' or 'sit and relax'?)
+    if (firstturn == "swim"):
+      print("AHH! This water is so cold!")
+      secondturn = input("I'm bored! Should I go somewhere else?")
+        if (secondturn == "no"):
+          break
+        elif (secondturn == "yes"):
+          thirdturn = input("Should I go to the 'city' or the 'park'?)
+            if (thirdturn == "city"):
+              print("Wow! It's so pretty!")
+              fourthturn == input("Should I 'walk around' or 'go out'?")
+                if (fourthturn == "walk around"):
+                print("That was soo tiring! Thanks for playing")
+                    break
+            elif (thirdturn == "park"):
+                print("It's so nice out today!")
+                fifthturn = input("Should I 'dance' or 'run around'")
+                    if (fifthturn == "dance"):
+                        print("That was soo tiring! Thanks for playing")
+                            break
+                    elif (fifthturn == "run around"):
+                        print("That was soo tiring! Thanks for playing")
+                            break
+    elif (firstturn == "sit and relax"):
+        sixthturn = input 
